8f43eb4fab579c1f6012dc48828fbde3719538ef
Rusu-Eduard <contact.rusueduard@gmail.com>
2026-04-09 19:56:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 async function loadLeaderboard(){
-
- const res = await 
-fetch("https://rusu-eduard.github.io/knowledge-league/leaderboard.json")
-
+ const res = await fetch("https://rusu-eduard.github.io/knowledge-league/leaderboard.json")
  const data = await res.json()
 
  const container = document.getElementById("leaderboard")
-
  container.innerHTML = ""
 
  data.teams.forEach(team=>{
-  container.innerHTML += `
-   <p>${team.name} - ${team.points}</p>
-  `
+  container.innerHTML += `<p>${team.name} - ${team.points}</p>`
  })
-
 }
 
 loadLeaderboard()
-
-setInterval(loadLeaderboard,5000)
+setInterval(loadLeaderboard, 5000)
